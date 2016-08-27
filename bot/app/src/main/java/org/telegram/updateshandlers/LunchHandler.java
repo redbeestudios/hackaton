d81bf56ac4b9022e788bf377fc22f54571ac41c1
ptonkli <pablo.tonkli@redb.ee>
2016-08-27 01:52:07 -0300

    docker

--- bot/app/src/main/java/org/telegram/updateshandlers/LunchHandler.java
+++ bot/app/src/main/java/org/telegram/updateshandlers/LunchHandler.java
@@ -1,18 +1,7 @@
 package org.telegram.updateshandlers;
 
-<<<<<<< HEAD
 import static org.telegram.services.LocalisationService.lformat;
-=======
-import io.redbee.utils.GroupingCollector;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.telegram.SenderHelper;
-import org.telegram.api.methods.Constants;
-import org.telegram.api.methods.SendMessage;
-import org.telegram.api.objects.Message;
-import org.telegram.api.objects.ReplyKeyboardMarkup;
-import org.telegram.services.BotLogger;
-import org.telegram.services.Emoji;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -31,8 +20,8 @@ import org.telegram.api.objects.Message;
 import org.telegram.api.objects.ReplyKeyboardMarkup;
 import org.telegram.services.BotLogger;
 import org.telegram.services.Emoji;
-import org.telegram.services.LocalisationService;
 
+import io.redbee.utils.GroupingCollector;
 import redis.clients.jedis.Jedis;
 
 /**
@@ -40,156 +29,158 @@ import redis.clients.jedis.Jedis;
  */
 public class LunchHandler extends BaseStatelessHandler {
 
-    private static final BotLogger LOGGER = BotLogger.getLogger(JenkinsHandlers.class.getName());
-    private List<String[]> actions = Arrays.asList(new String[]{"Restaurant", Emoji.CONSTRUCTION_SIGN.toString()},
-            new String[]{"Event", Emoji.BUS_STOP.toString()}, new String[]{"Order", Emoji.DELIVERY_TRUCK.toString()},new String[]{"Poll", Emoji.TELEVISION.toString()});
-	
-    private Jedis jedis;
+	private static final BotLogger LOGGER = BotLogger.getLogger(LunchHandler.class.getName());
+	private List<String[]> actions = Arrays.asList(new String[] { "Restaurant", Emoji.CONSTRUCTION_SIGN.toString() },
+			new String[] { "Event", Emoji.BUS_STOP.toString() },
+			new String[] { "Order", Emoji.DELIVERY_TRUCK.toString() },
+			new String[] { "Poll", Emoji.TELEVISION.toString() });
 
-    public LunchHandler(){
-        super();
-        jedis = new Jedis("localhost");
-    }
+	private Jedis jedis;
+	private Client client;
 
-    @Override
-    public List<String[]> getActions() {
+	public LunchHandler() {
+		super();
+		jedis = new Jedis("localhost");
+		client = ClientBuilder.newClient();
+	}
 
-        return this.actions;
-    }
+	@Override
+	public List<String[]> getActions() {
 
-    @Override
-    public ReplyKeyboardMarkup getDefaultKeyboard() {
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboad(false);
-        List<List<String>> keyboard = new ArrayList<>();
-        List<String> keyboardFirstRow = new ArrayList<>();
-        keyboardFirstRow.add(lformat(this.actions.get(0)));
-        keyboardFirstRow.add(lformat(this.actions.get(1)));
-        List<String> keyboardSecondRow = new ArrayList<>();
-        keyboardSecondRow.add(lformat(this.actions.get(2)));
-        keyboardSecondRow.add(lformat(this.actions.get(3)));
-        keyboard.add(keyboardFirstRow);
-        keyboard.add(keyboardSecondRow);
-        replyKeyboardMarkup.setKeyboard(keyboard);
+		return this.actions;
+	}
 
-        return replyKeyboardMarkup;
-    }
+	@Override
+	public ReplyKeyboardMarkup getDefaultKeyboard() {
+		ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+		replyKeyboardMarkup.setSelective(true);
+		replyKeyboardMarkup.setResizeKeyboard(true);
+		replyKeyboardMarkup.setOneTimeKeyboad(false);
+		List<List<String>> keyboard = new ArrayList<>();
+		List<String> keyboardFirstRow = new ArrayList<>();
+		keyboardFirstRow.add(lformat(this.actions.get(0)));
+		keyboardFirstRow.add(lformat(this.actions.get(1)));
+		List<String> keyboardSecondRow = new ArrayList<>();
+		keyboardSecondRow.add(lformat(this.actions.get(2)));
+		keyboardSecondRow.add(lformat(this.actions.get(3)));
+		keyboard.add(keyboardFirstRow);
+		keyboard.add(keyboardSecondRow);
+		replyKeyboardMarkup.setKeyboard(keyboard);
 
-    public ReplyKeyboardMarkup getActionKeyboards(List<String[]> actions){
+		return replyKeyboardMarkup;
+	}
 
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+	public ReplyKeyboardMarkup getActionKeyboards(List<String[]> actions) {
 
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboad(false);
+		ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
 
-        List<List<String>> keyboard = new ArrayList<>();
+		replyKeyboardMarkup.setSelective(true);
+		replyKeyboardMarkup.setResizeKeyboard(true);
+		replyKeyboardMarkup.setOneTimeKeyboad(false);
 
+		List<List<String>> keyboard = new ArrayList<>();
 
-        List<List<String[]>> pages = actions.stream().collect(new GroupingCollector<>(3));
+		List<List<String[]>> pages = actions.stream().collect(new GroupingCollector<>(3));
 
-        List<String> keyboardFirstRow = new ArrayList<>(0);
+		List<String> keyboardFirstRow = new ArrayList<>(0);
 
-        for(List<String[]> page : pages) {
-            keyboardFirstRow = new ArrayList<>();
-            for (String[] action : page) {
+		for (List<String[]> page : pages) {
+			keyboardFirstRow = new ArrayList<>();
+			for (String[] action : page) {
 
-                keyboardFirstRow.add(lformat(action));
-            }
-            keyboard.add(keyboardFirstRow);
-        }
-
-        replyKeyboardMarkup.setKeyboard(keyboard);
-
-        return replyKeyboardMarkup;
-
-    }
-
-    private SendMessage buildMessage(Message inputMsg , ReplyKeyboardMarkup keyboard ) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(inputMsg.getChatId());
-        sendMessage.enableMarkdown(true);
-        sendMessage.setReplayToMessageId(inputMsg.getMessageId());
-        sendMessage.setReplayMarkup(keyboard);
-        return sendMessage;
-    }
-
-    public SendMessage handleRestaurant(Message message) {
-        
-    	
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://demo5329197.mockable.io/restaurants");
-    	
-    	Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
-    	
-    	JSONArray restaurants = new JSONObject(response.readEntity(String.class)).getJSONArray("restaurants");
-    	
-    	List<String[]> restaurantActions = new ArrayList<>();
-    	for (int i = 0; i < restaurants.length(); i++) {
-    		JSONObject restaurant = restaurants.getJSONObject(i);
-    		restaurantActions.add(new String[] {restaurant.getString("name"),""});
+				keyboardFirstRow.add(lformat(action));
+			}
+			keyboard.add(keyboardFirstRow);
 		}
-    	
-    	jedis.set(message.getChatId().toString(), "1");
-    	
-    	SendMessage sendMessage = new SendMessage();
+
+		replyKeyboardMarkup.setKeyboard(keyboard);
+
+		return replyKeyboardMarkup;
+
+	}
+
+	private SendMessage buildMessage(Message inputMsg, ReplyKeyboardMarkup keyboard) {
+		SendMessage sendMessage = new SendMessage();
+		sendMessage.setChatId(inputMsg.getChatId());
+		sendMessage.enableMarkdown(true);
+		sendMessage.setReplayToMessageId(inputMsg.getMessageId());
+		sendMessage.setReplayMarkup(keyboard);
+		return sendMessage;
+	}
+
+	public SendMessage handleRestaurant(Message message) {
+
+		WebTarget target = client.target("http://demo5329197.mockable.io/restaurants");
+
+		Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
+
+		JSONArray restaurants = new JSONObject(response.readEntity(String.class)).getJSONArray("restaurants");
+
+		List<String[]> restaurantActions = new ArrayList<>();
+		for (int i = 0; i < restaurants.length(); i++) {
+			JSONObject restaurant = restaurants.getJSONObject(i);
+			restaurantActions.add(new String[] { restaurant.getString("name"), "" });
+		}
+
+		jedis.set(message.getChatId().toString(), "1");
+
+		SendMessage sendMessage = new SendMessage();
 		sendMessage.setText("Testttt");
 		sendMessage.setChatId(message.getChatId());
 		sendMessage.setReplayMarkup(getActionKeyboards(restaurantActions));
-		
+
 		return sendMessage;
-    }
+	}
 
-    
+	public SendMessage handleEvent(Message message) {
+		return buildMessage(message, getDefaultKeyboard());
+	}
 
-    public SendMessage handleEvent(Message message) {
-        return buildMessage(message, getDefaultKeyboard());
-    }
+	public SendMessage handlePoll(Message message) {
+		return buildMessage(message, getDefaultKeyboard());
+	}
 
-
-    public SendMessage handlePoll(Message message) {
-        return buildMessage(message, getDefaultKeyboard());
-    }
-
-    public SendMessage handleOrder(Message message) {
-        return buildMessage(message, getDefaultKeyboard());
-    }
+	public SendMessage handleOrder(Message message) {
+		return buildMessage(message, getDefaultKeyboard());
+	}
 
 	@Override
 	protected BotApiMethod doHandleMessage(Message message) {
+
 		
-		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://demo5329197.mockable.io/restaurants");
-    	Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
-    	JSONArray restaurants = new JSONObject(response.readEntity(String.class)).getJSONArray("restaurants");
-		
+		Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
+		JSONArray restaurants = new JSONObject(response.readEntity(String.class)).getJSONArray("restaurants");
+
 		String level = jedis.get(message.getChatId().toString());
 
 		List<String[]> dishesActions = new ArrayList<>();
 		if (level.equals("1")) {
 			for (int i = 0; i < restaurants.length(); i++) {
-	    		JSONObject restaurant = restaurants.getJSONObject(i);
-	    		if (restaurant.getString("name").equals(message.getText())) {
-	    			JSONArray dishes = restaurant.getJSONArray("dishes");
-	    			
-	    	    	for (int j = 0; j < dishes.length(); j++) {
-	    	    		JSONObject dish = dishes.getJSONObject(j);
-	    	    		dishesActions.add(new String[] {dish.getString("type"), Emoji.BLACK_NIB.toString()});
-	    			}
-	    			
-	    		}
+				JSONObject restaurant = restaurants.getJSONObject(i);
+				if (restaurant.getString("name").equals(message.getText())) {
+					JSONArray dishes = restaurant.getJSONArray("dishes");
+
+					for (int j = 0; j < dishes.length(); j++) {
+						JSONObject dish = dishes.getJSONObject(j);
+						dishesActions.add(new String[] { dish.getString("type"), Emoji.BLACK_NIB.toString() });
+					}
+
+				}
 			}
 		}
-		
-		jedis.set(message.getChatId().toString(), "2");
-    	
-    	SendMessage sendMessage = new SendMessage();
-		sendMessage.setText("Testttt");
-		sendMessage.setChatId(message.getChatId());
-		sendMessage.setReplayMarkup(getActionKeyboards(dishesActions));
-		
-		return sendMessage;
+
+		if (!dishesActions.isEmpty()) {
+
+			jedis.set(message.getChatId().toString(), "2");
+
+			SendMessage sendMessage = new SendMessage();
+			sendMessage.setText("Testttt");
+			sendMessage.setChatId(message.getChatId());
+			sendMessage.setReplayMarkup(getActionKeyboards(dishesActions));
+
+			return sendMessage;
+		}
+		return buildErrorMsg(message);
 	}
 }
