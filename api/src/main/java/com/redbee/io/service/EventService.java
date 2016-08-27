6c47d5f116bb10ec18ab2ecc8cba7621f7cb096c
manuelcueto <manuel.cueto@redb.ee>
2016-08-27 03:45:20 -0300

    me quiero ir

--- api/src/main/java/com/redbee/io/service/EventService.java
+++ api/src/main/java/com/redbee/io/service/EventService.java
@@ -4,9 +4,11 @@ package com.redbee.io.service;
 import com.redbee.io.persistence.entities.Event;
 import com.redbee.io.persistence.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EventService {
 
     @Autowired
