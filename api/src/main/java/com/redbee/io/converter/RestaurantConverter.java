02e08cedf19e27f69194df3832f8d085c87cb02e
gmarti28 <gmartin@richrelevance.com>
2016-10-21 18:25:56 -0300

    el id es opcional y se autogenera

--- api/src/main/java/com/redbee/io/converter/RestaurantConverter.java
+++ api/src/main/java/com/redbee/io/converter/RestaurantConverter.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by martin on 26/08/16.
@@ -20,21 +21,24 @@ public class RestaurantConverter {
         result.setId(restaurant.getId());
         result.setName(restaurant.getName());
         List<DishRepresentation> dishes = new ArrayList<DishRepresentation>();
-//        restaurant.getDishes().forEach(dish ->
-//                dishes.add(new DishRepresentation(dish.getName(), dish.getType()))
-//        );
+        restaurant.getDishes().forEach(dish ->
+                dishes.add(new DishRepresentation(dish.getName(), dish.getType()))
+        );
         result.setDishes(dishes);
         return result;
     }
 
     public Restaurant convert(RestaurantRepresentation restaurantRepresentation) {
+        //TODO: Tomar el ID de la Base
         Restaurant result = new Restaurant();
-        result.setId(restaurantRepresentation.getId());
+        if(Optional.ofNullable(restaurantRepresentation.getId()).isPresent()){
+            result.setId(restaurantRepresentation.getId());
+        }
         result.setName(restaurantRepresentation.getName());
         List<Dish> dishes = new ArrayList<Dish>();
-//        restaurantRepresentation.getDishes().forEach(dish ->
-//         dishes.add(new Dish(dish.getName(), dish.getType()))
-//        );
+        restaurantRepresentation.getDishes().forEach(dish ->
+         dishes.add(new Dish(dish.getName(), dish.getType()))
+        );
         result.setDishes(dishes);
         return result;
 
