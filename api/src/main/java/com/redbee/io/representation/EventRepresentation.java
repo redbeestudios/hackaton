648cba510edefd5b8dd85e2af785a65cf4f44369
Martin Britez <martin.britez@redb.ee>
2016-10-21 18:33:46 -0300

    eventRepresentation

---
+++ api/src/main/java/com/redbee/io/representation/EventRepresentation.java
@@ -0,0 +1,96 @@
+package com.redbee.io.representation;
+
+import com.redbee.io.persistence.entities.*;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by martin on 21/10/16.
+ */
+public class EventRepresentation {
+
+    @Id
+    private String id;
+
+    private Date date;
+
+    private List<Restaurant> restaurants;
+
+    private List<User> voters;
+
+    private Restaurant chosenRestaurant;
+
+    private List<Vote> votes;
+
+    private List<Order> orders;
+
+    private EventState state;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+
+    public void setRestaurants(List<Restaurant> restaurants) {
+        this.restaurants = restaurants;
+    }
+
+    public List<User> getVoters() {
+        return voters;
+    }
+
+    public void setVoters(List<User> voters) {
+        this.voters = voters;
+    }
+
+    public Restaurant getChosenRestaurant() {
+        return chosenRestaurant;
+    }
+
+    public void setChosenRestaurant(Restaurant chosenRestaurant) {
+        this.chosenRestaurant = chosenRestaurant;
+    }
+
+    public List<Vote> getVotes() {
+        return votes;
+    }
+
+    public void setVotes(List<Vote> votes) {
+        this.votes = votes;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public EventState getState() {
+        return state;
+    }
+
+    public void setState(EventState state) {
+        this.state = state;
+    }
+
+
+}
