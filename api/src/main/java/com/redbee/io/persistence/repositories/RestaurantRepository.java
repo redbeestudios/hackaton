64204a055e3ce7f47d98ad79cfe2a0a9bc6f73a8
Martin Britez <martin.britez@redb.ee>
2016-10-21 19:11:24 -0300

    eventResources

--- api/src/main/java/com/redbee/io/persistence/repositories/RestaurantRepository.java
+++ api/src/main/java/com/redbee/io/persistence/repositories/RestaurantRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RestaurantRepository extends MongoRepository<Restaurant, String> {
 
-//    Restaurant findByName(String name);
+    Restaurant findByName(String name);
 
 
 
