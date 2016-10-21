803ce8302487be6284424a5e22e642d00cf04eda
Martin Britez <martin.britez@redb.ee>
2016-10-21 19:02:10 -0300

    resources event

--- api/src/main/java/com/redbee/io/resources/EventResource.java
+++ api/src/main/java/com/redbee/io/resources/EventResource.java
@@ -3,6 +3,7 @@ package com.redbee.io.resources;
 import com.redbee.io.persistence.entities.Event;
 import com.redbee.io.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,10 @@ public class EventResource {
     public List<Event> list(){
         return eventService.getAll();
     }
+
+//    @RequestMapping(method = RequestMethod.GET,value = "/{id}")
+//    public Event getEvent(@PathVariable String id){
+//       return eventService.findOne(id);
+//    }
 
 }
