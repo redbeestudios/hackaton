a4be9ed43789a281ccb74e5f4a411ad651add424
manuelcueto <manuel.cueto@redb.ee>
2016-10-21 19:14:20 -0300

    hago andar el coso

--- hackatom-ui/hackatom-ui/dist/out-tsc/app/services/restaurant.srv.js
+++ hackatom-ui/hackatom-ui/dist/out-tsc/app/services/restaurant.srv.js
@@ -8,19 +8,39 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 import { Injectable } from '@angular/core';
-export var RestaurantService = (function () {
-    function RestaurantService() {
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+var RestaurantService = (function () {
+    function RestaurantService(http) {
+        this.http = http;
+        this.restaurantUrl = '/restaurants';
     }
     RestaurantService.prototype.getRestaurants = function () {
         return null;
     };
+    RestaurantService.prototype.getRestaurant = function (id) {
+        return this.http.get(this.restaurantUrl)
+            .map(this.extractData)
+            .catch(this.handleError);
+    };
+    RestaurantService.prototype.extractData = function (res) {
+        var body = res.json();
+        return body.data || {};
+    };
+    RestaurantService.prototype.handleError = function (error) {
+        var errMsg = (error.message) ? error.message :
+            error.status ? error.status + " - " + error.statusText : 'Server error';
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    };
     RestaurantService.prototype.saveRestaurants = function (restaurant) { };
     RestaurantService.prototype.deleteRestaurant = function (id) { };
     RestaurantService.prototype.editRestaurant = function (id) { };
-    RestaurantService = __decorate([
-        Injectable(), 
-        __metadata('design:paramtypes', [])
-    ], RestaurantService);
     return RestaurantService;
 }());
+RestaurantService = __decorate([
+    Injectable(),
+    __metadata("design:paramtypes", [Http])
+], RestaurantService);
+export { RestaurantService };
 //# sourceMappingURL=../../../../src/app/services/restaurant.srv.js.map
--- hackatom-ui/hackatom-ui/src/app/services/restaurant.srv.ts
+++ hackatom-ui/hackatom-ui/src/app/services/restaurant.srv.ts
@@ -1,13 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Restaurant } from '../domain/restaurant';
+import { Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
 
 @Injectable()
 export class RestaurantService {
+  private restaurantUrl = '/restaurants';
+
+  constructor(private http: Http) {}
 
   getRestaurants(): Array<Restaurant> {
     return null;
   }
+
+  getRestaurant(id: String): Observable<Restaurant> {
+
+    return this.http.get(this.restaurantUrl)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+
+  }
+  private extractData(res: Response) {
+    let body = res.json();
+
+    return body.data || {};
+  }
+
+  private handleError (error: any) {
+      // In a real world app, we might use a remote logging infrastructure
+      // We'd also dig deeper into the error to get a better message
+      let errMsg = (error.message) ? error.message :
+        error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+      console.error(errMsg); // log to console instead
+      return Observable.throw(errMsg);
+    }
 
   saveRestaurants(restaurant: Restaurant): void {}
 
