a0f087bc338cd51cc96d2ef35487e427e92197ee
GFibrizo <zebas.graffe@gmail.com>
2016-10-21 19:17:04 -0300

    Agregados metodos a EventService. Agregado al RestaurantConverter metodo para convertir lista de rest

--- api/src/main/java/com/redbee/io/service/EventService.java
+++ api/src/main/java/com/redbee/io/service/EventService.java
@@ -1,8 +1,10 @@
 package com.redbee.io.service;
 
 
+import com.redbee.io.converter.EventConverter;
 import com.redbee.io.persistence.entities.Event;
 import com.redbee.io.persistence.repositories.EventRepository;
+import com.redbee.io.representation.EventRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +23,16 @@ public class EventService {
         return repo.findAll();
     }
 
-    public EventRepository create(EventRepository eventRespresentation) {
+    public Event get(String id) {
+        return repo.findOne(id);
+    }
+    
+    public EventRepresentation create(EventRepresentation eventRespresentation) {
         Event Event = converter.convert(eventRespresentation);
         return converter.convert(repo.insert(Event));
     }
 
-    public void update(EventRepository representation) {
+    public void update(EventRepresentation representation) {
         Event Event = converter.convert(representation);
         repo.save(Event);
     }
