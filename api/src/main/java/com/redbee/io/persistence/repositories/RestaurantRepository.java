611bad10a50463ac8dd383567db0c1fb1e5af341
GFibrizo <zebas.graffe@gmail.com>
2016-10-21 20:46:12 -0300

    Cambios a los repositories. No tienen que tener metodos. Heredan todo del MongoRepo

--- api/src/main/java/com/redbee/io/persistence/repositories/RestaurantRepository.java
+++ api/src/main/java/com/redbee/io/persistence/repositories/RestaurantRepository.java
@@ -5,9 +5,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RestaurantRepository extends MongoRepository<Restaurant, String> {
 
-    Restaurant findByName(String name);
-
-
-
-
 }
