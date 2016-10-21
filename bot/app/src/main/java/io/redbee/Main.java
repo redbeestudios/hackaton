eca91eaddfe7275a96d26b5689f694389b0b8f2b
Hervé Odiard <herve@evreh>
2016-10-21 20:42:13 -0300

    Obtener voto desde respuesta. Simplificado LunchHandler

--- bot/app/src/main/java/io/redbee/Main.java
+++ bot/app/src/main/java/io/redbee/Main.java
@@ -1,12 +1,16 @@
 package io.redbee;
 
+import org.telegram.SenderHelper;
+import org.telegram.api.methods.Constants;
 import org.telegram.updateshandlers.LunchHandler;
 import org.telegram.updateshandlers.UpdatesCallback;
+import org.telegram.updatesreceivers.UpdatesThread;
 
 public class Main {
 
     public static void main(String[] args) {
-        UpdatesCallback lunchHandler = new LunchHandler();
+      UpdatesThread updatesThread = new UpdatesThread(Constants.BOT_TOKEN, new LunchHandler());
+      SenderHelper.SendWebhook("", Constants.BOT_TOKEN);
     }
 
 }
