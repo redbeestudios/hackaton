be86d4a12f1fe98cc41c8947fe71871b323f23a1
Martin Britez <martin.britez@redb.ee>
2016-10-22 01:25:39 -0300

    .

--- api/src/main/java/com/redbee/io/Application.java
+++ api/src/main/java/com/redbee/io/Application.java
@@ -1,7 +1,11 @@
 package com.redbee.io;
 
+import com.redbee.io.filter.CORSFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
 /**
  * Created by Channo on 14/12/15.
@@ -11,6 +15,20 @@ public class Application {
 
     public static void main(String[] args)  {
         SpringApplication.run(Application.class, args);
+    }
+
+    public void configure(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                .and()
+
+                .addFilterBefore(new CORSFilter(), AbstractPreAuthenticatedProcessingFilter.class)
+                .csrf().disable()
+
+                .authorizeRequests().anyRequest().permitAll();
+
     }
 
 }
