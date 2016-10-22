5e0a98560a1addd25ae601cc15ad0fcd1dbd54a3
Martin Britez <martin.britez@redb.ee>
2016-10-22 01:38:41 -0300

    disable security

--- api/src/main/java/com/redbee/io/Application.java
+++ api/src/main/java/com/redbee/io/Application.java
@@ -2,6 +2,7 @@ package com.redbee.io;
 
 import com.redbee.io.filter.CORSFilter;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +12,9 @@ import org.springframework.security.web.authentication.preauth.AbstractPreAuthen
  * Created by Channo on 14/12/15.
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args)  {
